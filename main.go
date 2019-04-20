c7ccbf41cb486e48bb294ea13a5da9fd2a260bda
masterdak <masterdak@mail.ru>
2019-04-20 16:40:35 +0400

    1.2

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"neuron/nr"
 )
 
 func main() {
-	nr.CreateLayer([]int{2, 1})
+	nr.CreateLayer([]int{9, 8, 4, 3})
 	nr.NCorrect = 0.1
-	nr.SetAnswers([]float64{0.5})
 
-	for x := 0; x < 100; x++ {
-		nr.Calc()
-		fmt.Println(nr.Layers[1][0].Err)
+	for x := 0; x < 10000; x++ {
 	}
-
 }
