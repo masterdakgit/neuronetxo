339a8d998ec3ae6652356050cc35838cab257103
masterdak <masterdak@mail.ru>
2019-04-20 16:31:25 +0400

    1.2

--- main.go
+++ main.go
@@ -2,162 +2,17 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
 	"neuron/nr"
 )
 
-const (
-	nc = 0.1
-
-	n0 = 3
-	n1 = 9
-	n2 = 1
-)
-
-var (
-	NS0 [n0 + 1]Neuron
-	NS1 [n1 + 1]Neuron
-	NS2 [n2]Neuron
-
-	W0 [n0 + 1][n1 + 1]float64
-	W1 [n1 + 1][n2 + 1]float64
-)
-
-type Neuron struct {
-	in, out, err float64
-}
-
 func main() {
 	nr.CreateLayer([]int{2, 1})
+	nr.NCorrect = 0.1
+	nr.SetAnswers([]float64{0.5})
 
-	for l := range nr.Layers {
-		for n := range nr.Layers[l] {
-			fmt.Print(nr.Layers[l][n].Out, " ")
-		}
-		fmt.Println()
+	for x := 0; x < 100; x++ {
+		nr.Calc()
+		fmt.Println(nr.Layers[1][0].Err)
 	}
 
-	for w := range nr.Weight {
-		for n := range nr.Weight[w] {
-			for x := range nr.Weight[w][n] {
-				fmt.Printf("%.2f", nr.Weight[w][n][x])
-				fmt.Print(" ")
-			}
-			fmt.Print("| ")
-		}
-		fmt.Println()
-	}
-
-	nr.CalcWeight()
-	fmt.Println()
-	fmt.Println(nr.Layers[1][0].In)
-
-	/*
-		rand.Seed(time.Now().UnixNano())
-		StartWeight()
-
-		for n := 0; n < 100000; n++{
-			InputPrepare()
-			Calculate()
-		}
-
-		for{
-			InputPrepare()
-			Calculate()
-
-			fmt.Println(Answer())
-			for n := 0; n < n2; n++{
-				fmt.Print(NS2[n].out, " ")
-			}
-
-
-			s := 0
-			fmt.Scanln(&s)
-		}*/
-}
-
-func InputPrepare() {
-	for x := 0; x < n0; x++ {
-		NS0[x].out = float64(rand.Intn(2))
-	}
-}
-
-func StartWeight() {
-	for x0 := 0; x0 < n0+1; x0++ {
-		for x1 := 0; x1 < n1; x1++ {
-			W0[x0][x1] = float64((rand.Intn(1000) - 500) / 1000)
-		}
-	}
-	for x0 := 0; x0 < n1+1; x0++ {
-		for x1 := 0; x1 < n2; x1++ {
-			W1[x0][x1] = float64((rand.Intn(1000) - 500) / 1000)
-		}
-	}
-
-	NS0[n0].out = 1
-	NS1[n1].out = 1
-
-}
-
-func Calculate() {
-	//Подсчет входов слой NS1
-	for x1 := 0; x1 < n1; x1++ {
-		NS1[x1].in = 0
-		for x0 := 0; x0 < n0+1; x0++ {
-			NS1[x1].in += NS0[x0].out * W0[x0][x1]
-		}
-		NS1[x1].out = Activate(NS1[x1].in)
-	}
-
-	//Подсчет входов слой NS2 и ошибки
-	for x2 := 0; x2 < n2; x2++ {
-		NS2[x2].in = 0
-		for x1 := 0; x1 < n1+1; x1++ {
-			NS2[x2].in += NS1[x1].out * W1[x1][x2]
-		}
-		NS2[x2].out = Activate(NS2[x2].in)
-		NS2[x2].err = Answer() - NS2[x2].out
-	}
-
-	//Подсчет ошибки для слоя NS1
-	for x1 := 0; x1 < n1+1; x1++ {
-		NS1[x1].err = 0
-		for x2 := 0; x2 < n2; x2++ {
-			NS1[x1].err += NS2[x2].err * W1[x1][x2]
-		}
-	}
-
-	Correct()
-}
-
-func Correct() {
-	for x1 := 0; x1 < n1; x1++ {
-		for x0 := 0; x0 < n0+1; x0++ {
-			W0[x0][x1] += WeightChange(NS0[x0], NS1[x1])
-		}
-	}
-	for x1 := 0; x1 < n1+1; x1++ {
-		for x0 := 0; x0 < n2; x0++ {
-			W1[x1][x0] += WeightChange(NS1[x1], NS2[x0])
-		}
-	}
-
-}
-
-func WeightChange(NLeft, NRight Neuron) float64 {
-	return nc * NRight.err * NRight.out * (1 - NRight.out) * NLeft.out
-}
-
-func Activate(x float64) float64 {
-	return 1 / (1 + math.Exp(-x))
-}
-
-func Answer() float64 {
-	a := float64(0)
-	for n := 0; n < n0; n++ {
-		a += NS0[n].out
-	}
-
-	return a / 3
 }
--- nr/lib.go
+++ nr/lib.go
@@ -3,12 +3,13 @@ package nr
 import (
 	"math"
 	"math/rand"
-	"time"
 )
 
 var (
-	Layers [][]Neuron
-	Weight [][][]float64
+	Layers   [][]Neuron
+	Weight   [][][]float64
+	NCorrect float64
+	Answers  []float64
 )
 
 type Neuron struct {
@@ -16,6 +17,7 @@ type Neuron struct {
 }
 
 func CreateLayer(L []int) {
+	NCorrect = 0.1
 	Layers = make([][]Neuron, len(L))
 	for n := 0; n < len(L); n++ {
 		Layers[n] = make([]Neuron, L[n]+1)
@@ -26,7 +28,6 @@ func CreateLayer(L []int) {
 }
 
 func startWeight() {
-	rand.Seed(time.Now().UnixNano())
 	Weight = make([][][]float64, len(Layers)-1)
 
 	for w := range Weight {
@@ -40,22 +41,64 @@ func startWeight() {
 			}
 		}
 	}
-	//Убираем нейрон смещения у последнего слоя
-	Layers[len(Layers)-1] = make([]Neuron, len(Layers[len(Layers)-1])-1)
 }
 
-func CalcWeight() {
+func Calc() {
 	for L := 0; L < len(Layers)-1; L++ {
-		for xr := 0; xr < len(Layers[L+1]); xr++ {
+		for xr := 0; xr < len(Layers[L+1])-1; xr++ {
 			Layers[L+1][xr].In = 0
-			for xl := 0; xl < len(Weight[L]); xl++ {
+			for xl := 0; xl < len(Layers[L]); xl++ {
 				Layers[L+1][xr].In += Layers[L][xl].Out * Weight[L][xl][xr]
+				Layers[L+1][xr].Activate()
 			}
 		}
 	}
-
+	layerError()
 }
 
-func (n *Neuron) Activae() {
+func layerError() {
+	Nr := len(Layers) - 1
+	for xr := 0; xr < len(Layers[Nr])-1; xr++ {
+		Layers[Nr][xr].Err = answer(xr) - Layers[Nr][xr].Out
+	}
+
+	for left := Nr - 1; left > 0; left-- {
+		right := left + 1
+		for xl := 0; xl < len(Layers[left])-1; xl++ {
+			Layers[left][xl].Err = 0
+			for xr := 0; xr < len(Layers[right])-1; xr++ {
+				Layers[left][xl].Err += Layers[right][xr].Err * Weight[left][xl][xr]
+			}
+		}
+	}
+	weightCorrect()
+}
+
+func weightCorrect() {
+	for w := 0; w < len(Weight); w++ {
+		for xl := 0; xl < len(Layers[w]); xl++ {
+			for xr := 0; xr < len(Layers[w+1])-1; xr++ {
+				Weight[w][xl][xr] += weightChange(Layers[w][xl], Layers[w+1][xr])
+			}
+		}
+	}
+}
+
+func weightChange(NLeft, NRight Neuron) float64 {
+	return NCorrect * NRight.Err * NRight.Out * (1 - NRight.Out) * NLeft.Out
+}
+
+func SetAnswers(a []float64) {
+	Answers = make([]float64, len(a))
+	for n := 0; n < len(a); n++ {
+		Answers[n] = a[n]
+	}
+}
+
+func answer(n int) float64 {
+	return Answers[n]
+}
+
+func (n *Neuron) Activate() {
 	n.Out = 1 / (1 + math.Exp(-n.In))
 }
